b61b51b687ac7185c92fb7c7eddc540c09ba97c8
a1837594 <88073327+a1837594@users.noreply.github.com>
2023-08-13 16:24:06 +0930

    Add files via upload

---
+++ Truckloads.cpp
@@ -0,0 +1,19 @@
+#include "Truckloads.h"
+#include <iostream>
+#include <cmath>
+
+int Truckloads::numTrucks(int numCrates, int loadSize, int stat){
+    if(numCrates<=loadSize){
+        return stat;
+    }
+    else{
+        numCrates=numCrates/2;
+        if(numCrates%1!=0) numCrates=static_cast<int>(std::ceil(numCrates));
+        stat++;
+        return numTrucks(numCrates,loadSize,stat);
+    }
+}//第一个是一共多少捆 第二个一辆车最多装几捆
+
+int Truckloads::numTrucks(int numCrates, int loadSize){
+    return numTrucks(numCrates,loadSize,1);
+}
---
+++ Truckloads.h
@@ -0,0 +1,9 @@
+#ifndef TRUCKLOADS_H
+#define TRUCKLOADS_H
+
+class Truckloads{
+    int numTrucks(int numCrates, int loadSize);
+    int numTrucks(int numCrates, int loadSize,int stat);
+};
+
+#endif
