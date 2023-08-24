0d0414e15d38b71fc417796f8c20ad70e8d0d1ff
a1837594 <88073327+a1837594@users.noreply.github.com>
2023-08-24 12:59:44 +0930

    Add files via upload

--- Finder.cpp
+++ Finder.cpp
@@ -3,10 +3,20 @@ using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
     for(size_t i = 1; i <= s2.size(); i++) {
-        size_t found = s1.find(s2.substr(0, i),i);
-        if (found != string::npos) {
-            result.push_back(found);
-        } else {
+        // size_t found = s1.find(s2.substr(0, i));
+        // if (found != string::npos) {
+        //     result.push_back(found);
+        // } else {
+        //     result.push_back(-1);
+        //     break;
+        // }
+        if (const auto it = std::search(s1.begin(), s1.end(),
+            std::boyer_moore_searcher(s2.begin(), s2.end()));
+        it != s1.end()
+        ){
+            result.push_back(it - s1.begin());
+        }
+        else{
             result.push_back(-1);
             break;
         }
--- Finder.h
+++ Finder.h
@@ -1,9 +1,14 @@
 #ifndef FINDER_H
 #define FINDER_H
+// #include <iostream>
+// #include <string>
+// #include <vector>
+// #include <chrono>
+#include <algorithm>
+#include <functional>
+#include <iomanip>
 #include <iostream>
-#include <string>
-#include <vector>
-#include <chrono>
+#include <string_view>
 class Finder {
 public:
 std::vector<int> findSubstrings(std::string s1, std::string s2);
