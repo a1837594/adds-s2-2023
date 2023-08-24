fd4ee81443c34eba00b2763da9a1cddbb2aaf61e
a1837594 <88073327+a1837594@users.noreply.github.com>
2023-08-24 13:49:10 +0930

    Add files via upload

--- Finder.cpp
+++ Finder.cpp
@@ -1,4 +1,5 @@
 #include "Finder.h"
+#include <iostream>
 using namespace std;
 vector<int> Finder::findSubstrings(string s1, string s2) {
     vector<int> result;
@@ -6,7 +7,7 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
     for(size_t i = 1; i <= s2.size(); i++) {
         size_t found = s1.find(s2.substr(0, i), start);
         if (found == string::npos) {
-            for(size_t j  = i; j < s2.size(); j++) {
+            for(size_t j  = i; j <= s2.size(); j++) {
                 result.push_back(-1);
             }
             break;
@@ -18,3 +19,16 @@ vector<int> Finder::findSubstrings(string s1, string s2) {
    }
     return result;
 }
+
+
+// int main(void) {
+//     Finder f; 
+//     for (int i: f.findSubstrings("7562976595726958629", "9588"))
+//         cout << i << "\t";
+//     cout << endl;
+    
+//     for (int i: f.findSubstrings("734658732465", "463"))
+//         cout << i << "\t";
+//     cout << endl;
+//     return 0;
+// }
