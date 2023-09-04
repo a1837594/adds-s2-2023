01e1b8a8f4d1c5eb68158de475cc2b1144b461ff
a1837594 <88073327+a1837594@users.noreply.github.com>
2023-09-04 17:27:56 +0930

    Add files via upload

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,7 +5,9 @@ std::vector<int> BubbleSort::sort(std::vector<int> list) {
     for(int i=n-1;i>1;i--){
         for(int j=0;j<i;j++){
             if(list.at(j)>list.at(j+1)){
-                std::swap(list[j],list[j+1]);
+                int save=list.at(j+1);
+                list.at(j)=list.at(j+1);
+                list.at(j+1)=save;
             }
         }
     }
